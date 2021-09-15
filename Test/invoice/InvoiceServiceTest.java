4088f587d4e6a8cf1d0854227bd2bcbd72d6bfa3
afreen505 <afreen.fathimaa9876@gmail.com>
2021-09-15 23:13:58 +0530

    Modified:Program modified to add MultipleRides Program

--- Test/invoice/InvoiceServiceTest.java
+++ Test/invoice/InvoiceServiceTest.java
@@ -7,13 +7,31 @@ public class InvoiceServiceTest {
 
     @Test
     public void whenGivenDistanceAndTimeShouldReturnTotalFare() {
-
         InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double totalFare = cabInvoiceGenerator.calculateFare(distance, time);
         System.out.println("Total Fare = " + totalFare);
         Assert.assertEquals(25, totalFare, 0.0);
+    }
 
+    @Test
+    public void whenGivenLessDistanceOrTimeShouldReturnMinimumFare() {
+        InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = cabInvoiceGenerator.calculateFare(distance, time);
+        System.out.println("Minimum Fare = " + fare);
+        Assert.assertEquals(5, fare, 0.0);
+    }
+
+    @Test
+    public void whenGivenMultipleRidesShouldReturnInvoiceSummary() {
+        InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+        double totalFare = cabInvoiceGenerator.calculateTotalFare(rides);
+        System.out.println("Total Fare = " + totalFare);
+        Assert.assertEquals(30, totalFare, 0.0);
     }
 }
--- main/invoice/InvoiceGenerator.java
+++ main/invoice/InvoiceGenerator.java
@@ -4,9 +4,18 @@ public class InvoiceGenerator {
 
     private static final int COST_PER_MINUTE = 1;
     private static final double MINIMUM_COSt_PER_KILOMETER = 10;
+    private static final double MINIMUM_FARE = 5.0;
 
     public double calculateFare(double distance, int time) {
+        double totalFare = distance * MINIMUM_COSt_PER_KILOMETER + time * COST_PER_MINUTE;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
 
-        return distance * MINIMUM_COSt_PER_KILOMETER + time * COST_PER_MINUTE;
+    public double calculateTotalFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
     }
 }
